b879168be0583c6a4098dba5fd224321cde52dba
alexmcdermid <alexander.mcdermid@gmail.com>
2021-11-14 12:46:23 -0800

    more styling and logic for save searches

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,7 +1,7 @@
 export default function Result(props){
     return(
         <div className="resultItem">
-             <button className='buttonLink' onClick={()=>{console.log(123)}}>{props.word},&nbsp;</button>
+             <button className='buttonLink' onClick={()=>{console.log(`add item ${props.word} to list of words to save`)}}>{props.word},&nbsp;</button>
         </div>
     )
 }
