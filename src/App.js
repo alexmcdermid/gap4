f7694e5767f4ae76aa71067950008172901235d2
alexmcdermid <alexander.mcdermid@gmail.com>
2021-11-13 16:09:41 -0800

    centering notebook icon

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
         <Navbar bg="light" expand="lg" fixed='bottom'>
           <Container fluid>
           <Nav.Link href="#linkSearch"><div className='bottomNavElement'><FontAwesomeIcon icon={faSearch} size="2x"/>Search</div></Nav.Link>
-          <Nav.Link href="#linkNote"><div className='bottomNavElement'><FontAwesomeIcon icon={faEdit} size="2x"/>Notebook</div></Nav.Link>
+          <Nav.Link href="#linkNote"><div className='bottomNavElement'><div className='notebook'><FontAwesomeIcon icon={faEdit} size="2x"/></div>Notebook</div></Nav.Link>
           <Nav.Link href="#linkSaved"><div className='bottomNavElement'><FontAwesomeIcon icon={faHeart} size="2x"/>Saved</div></Nav.Link>
           <Nav.Link href="#linkProfile"><div className='bottomNavElement'><FontAwesomeIcon icon={faUser} size="2x"/>Profile</div></Nav.Link>
           </Container>
