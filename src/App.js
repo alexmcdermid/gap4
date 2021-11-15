74894b34ae83919b3f6e866de7a180d49f58aa50
alexmcdermid <alexander.mcdermid@gmail.com>
2021-11-14 20:27:29 -0800

    toast to display words to save

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import './App.css';
 import Search from './components/Search/Search'
 import Result from './components/Result/Result';
-import { Navbar,Container,Nav } from 'react-bootstrap';
+import { Navbar,Container,Nav, ToastContainer, Toast, ToastHeader, ToastBody } from 'react-bootstrap';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faSearch,faHeart,faEdit,faUser} from "@fortawesome/free-solid-svg-icons"
 
@@ -30,7 +30,7 @@ class App extends Component {
       })
     } else {
       for (let i = 0; i<tempArr.length; i++) {
-        if (tempArr[i] == word) tempArr.splice(i,1)
+        if (tempArr[i] === word) tempArr.splice(i,1)
       }
       this.setState({
         wordsToSave:tempArr
@@ -49,8 +49,22 @@ class App extends Component {
         {this.state.filters==null ? <></>:<>Filter: {this.state.filters}</> }
         <br/>
         <div className="welcomeUser">Welcome userX</div>
+        {/* results */}
         {this.state.data!=null ?  <><br/> Results: {this.state.data.length} Words
         <div className='results'><Result data={this.state.data} handleWordSave={this.handleWordSave} wordsToSave={this.state.wordsToSave}/></div></> : <></>}
+        {/* words to save toast */}
+        {this.state.wordsToSave.length > 0 ? 
+        <ToastContainer position="top-center" className="p-3">
+          <Toast>
+            <Toast.Header>
+              <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+              <strong className="me-auto">Words to save</strong>
+            </Toast.Header>
+            <Toast.Body>{this.state.wordsToSave.map(function(word){return(<>{word},&nbsp;</>)})}</Toast.Body>
+          </Toast>
+        </ToastContainer>
+       : <div/>}
+
         <Navbar bg="light" expand="lg" fixed='bottom'>
           <Container fluid>
           <Nav.Link href="#linkSearch"><div className='bottomNavElement'><FontAwesomeIcon icon={faSearch} size="2x"/>Search</div></Nav.Link>
