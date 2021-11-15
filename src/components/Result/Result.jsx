04513c1693466581ea4a17f4e18d7298d303d9a7
alexmcdermid <alexander.mcdermid@gmail.com>
2021-11-14 20:58:30 -0800

    animations added to words to save when added to list tos ave

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -2,7 +2,7 @@ export default function Result(props){
     return(
         <div className="resultItem">
             {props.data.map(function(item,index){return(
-                <button className='buttonLink' key={index} onClick={()=>{props.handleWordSave(item.word)}} >{item.word},&nbsp;</button>
+                <button className='buttonLink' key={index} onClick={()=>{props.handleWordSave(item.word)}} style={props.wordsToSave.includes(item.word) ? {color:'red'} : {color:'black'}}>{item.word},&nbsp;</button>
             )})}
         </div>
     )
