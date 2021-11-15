8d5544ee634e3d1a57b972bb8c12e03f5c6d4e93
alexmcdermid <alexander.mcdermid@gmail.com>
2021-11-14 20:07:23 -0800

    change result back to function

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,18 +1,9 @@
-import { Component } from "react"
-
-class Result extends Component {
-    changeColor(){
-        console.log(123)
-    }
-    render(){
-        return(
-            <div className="resultItem">
-                {this.props.data.map(function(item,index){return(
-                    <button className='buttonLink' key={index} onClick={()=>{this.props.handleWordSave(item.word)}}>{item.word},&nbsp;</button>
-                )})}
-            </div>
-        )
+export default function Result(props){
+    return(
+        <div className="resultItem">
+            {props.data.map(function(item,index){return(
+                <button className='buttonLink' key={index} onClick={()=>{props.handleWordSave(item.word)}} >{item.word},&nbsp;</button>
+            )})}
+        </div>
+    )
 }
-}
-
-export default Result;
