9041d6ded4062cdabbc3f65f885c4e12bb21eeea
alexmcdermid <alexander.mcdermid@gmail.com>
2021-11-16 12:06:55 -0800

    remove rhyme time home from app.js

--- src/App.js
+++ src/App.js
@@ -17,9 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <br />
-        <div className='topText'>RHYME TIME HOME</div>
-        <br />
+     
 
         <BrowserRouter>
           <Switch>
