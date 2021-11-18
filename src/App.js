3fccb93740037b625d2ebc6c571e06c08bc45d43
alexmcdermid <alexander.mcdermid@gmail.com>
2021-11-18 15:15:02 -0800

    increase navbar text size

--- src/App.js
+++ src/App.js
@@ -97,13 +97,13 @@ class App extends Component {
             {this.state.user!=null  ?
             <> 
             <Nav.Link href="/notebook"><div className='bottomNavElement'><div className='notebook'><FontAwesomeIcon icon={faEdit} size="2x" /></div>Notebook</div></Nav.Link>
-            <Nav.Link href="/saved"><div className='bottomNavElement'><FontAwesomeIcon icon={faHeart} size="2x" />Saved</div></Nav.Link>
-            <Nav.Link href="/profile"><div className='bottomNavElement'><FontAwesomeIcon icon={faUser} size="2x" />Profile</div></Nav.Link>
+            <Nav.Link href="/saved"><div className='bottomNavElement'><FontAwesomeIcon icon={faHeart} size="2x" style={{marginLeft:'5%'}}/>Saved</div></Nav.Link>
+            <Nav.Link href="/profile"><div className='bottomNavElement'><FontAwesomeIcon icon={faUser} size="2x" style={{marginLeft:'2%'}}/>Profile</div></Nav.Link>
             </>
             :
             <>
             <Nav.Link href="/login"><div className='bottomNavElement'><div className='notebook'><FontAwesomeIcon icon={faEdit} size="2x" /></div>Notebook</div></Nav.Link>
-            <Nav.Link href="/login"><div className='bottomNavElement'><FontAwesomeIcon icon={faHeart} size="2x" />Saved</div></Nav.Link>
+            <Nav.Link href="/login"><div className='bottomNavElement'><FontAwesomeIcon icon={faHeart} size="2x" style={{marginLeft:'5%'}}/>Saved</div></Nav.Link>
             <Nav.Link href="/login"><div className='bottomNavElement'><FontAwesomeIcon icon={faUserPlus} size="2x" style={{marginLeft:'30%'}}/>Login</div></Nav.Link>
             </>
             }
